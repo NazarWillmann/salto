635d85307a82459703a80bb6e3f6ac6659c821e4
NazarWillmann <80369136+NazarWillmann@users.noreply.github.com>
2024-11-11 12:24:01 +0300

    Delete qa-utils/src/main/java/ooo/jtc/db directory

--- qa-utils/src/main/java/ooo/jtc/db/DbConnectionInfo.kt
+++
@@ -1,6 +0,0 @@
-package ooo.jtc.db
-
-/**
- * Data class for store db connection info
- */
-data class DbConnectionInfo(val host: String, val port: Int, val user: String, val password: String)
--- qa-utils/src/main/java/ooo/jtc/db/DbSchema.kt
+++
@@ -1,9 +0,0 @@
-package ooo.jtc.db
-
-/**
- * Схема в ДБ.
- * В планах добавть взаимодействие с таблицами для DSL
- */
-interface DbSchema {
-    val dbSchemaName: String
-}
